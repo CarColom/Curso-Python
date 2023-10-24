980002ed82a9dcd2892fc7903df2d0964dab8c0c
CarColom <carlaycolom@gmail.com>
2023-10-24 12:19:39 -0300

    Curso_Completado

---
+++ condicionales/if-else.py
@@ -0,0 +1,9 @@
+
+contraseña_almacenada = "CarColom"
+contraseña_escrita = '''CarColom'''
+
+if contraseña_almacenada == contraseña_escrita:
+    print("INICIANDO SESIÒN...")
+else: 
+    print("CONTRASEÑA EQUIVOCADA, INTENTE DE NUEVO")    
+    
